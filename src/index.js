af6c04191838f097d874175f6755c4eab253b9b2
Marcel Schmitz <schmitzoide@gmail.com>
2022-11-04 21:48:34 +0000

    added plugins sidebar

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 /**
  * External dependencies
  */
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType } from "@wordpress/blocks";
+import { registerPlugin } from "@wordpress/plugins";
 
 /**
  * Internal dependencies
  */
-import Edit from './edit';
-import Save from './save';
-import block from '../block.json';
+import Edit from "./edit";
+import Save from "./save";
+import block from "../block.json";
+import Fields from "./components/general-sidebar-example";
 
-registerBlockType( block, {
-	edit: Edit,
-	save: Save,
-} );
+registerBlockType(block, {
+    edit: Edit,
+    save: Save,
+});
+
+registerPlugin("pl-gutenberg-boilerplate", {
+    render() {
+        return <Sidebar />;
+    },
+});
